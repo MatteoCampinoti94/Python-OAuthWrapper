f3585b828d6b7a561bc6f2ed1ad4080712292144
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-05-13 16:28:08 +0200

    Get user and oauth keys from .conf file

--- backup.py
+++ backup.py
@@ -1,1 +1,29 @@
+import requests
+import json
+import os
 
+user = str()
+oauth_key = str()
+oauth_sec = str()
+
+if os.path.isfile('backup.conf'):
+    with open('backup.conf') as conf:
+        conf = [c.strip() for c in conf.readlines()]
+
+    for c in conf:
+        c = c.replace(' ', '')
+        if c.startswith('user='):
+            user = c[5:]
+        if c.startswith('oauth_key='):
+            oauth_key = c[11:]
+        if c.startswith('oauth_sec='):
+            oauth_sec = c[11:]
+
+if not user:
+    print('ERROR: user not defined')
+if not oauth_key:
+    print('ERROR: oauth key not defined')
+if not oauth_sec:
+    print('ERROR: oauth secret key not defined')
+if not user or not oauth_key or not oauth_sec:
+    return 1
