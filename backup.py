1557548928688e821cec58abcbe098959a6d9f35
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-05-13 16:53:20 +0200

    Comments and small changes

--- backup.py
+++ backup.py
@@ -2,23 +2,26 @@ import requests
 import json
 import os, sys
 
+# Variable declaration
 user = str()
 oauth_key = str()
 oauth_sec = str()
 
+# If file exsts read all lines, strip and remove spaces
 if os.path.isfile('backup.conf'):
     with open('backup.conf') as conf:
-        conf = [c.strip() for c in conf.readlines()]
+        conf = [c.strip().replace(' ', '') for c in conf.readlines()]
 
+    # Check each read line and split them around the = sign if they starts with one of the settings
     for c in conf:
-        c = c.replace(' ', '')
         if c.startswith('user='):
-            user = "".join(c.split('=')[1:])
+            user = c.split('=')[1]
         if c.startswith('oauth_key='):
-            oauth_key = "".join(c.split('=')[1:])
+            oauth_key = c.split('=')[1]
         if c.startswith('oauth_sec='):
-            oauth_sec = "".join(c.split('=')[1:])
+            oauth_sec = c.split('=')[1]
 
+# Print which variables remained empty and exit if any is
 if not user:
     print('ERROR: user not defined')
 if not oauth_key:
@@ -32,6 +35,7 @@ else:
     print('oauth_key =', oauth_key)
     print('oauth_sec =', oauth_sec)
 
+# Get likes using api url and print response and message
 likes = f"http://api.tumblr.com/v2/blog/{user}.tumblr.com/likes?api_key={oauth_key}"
 likes = requests.get(likes)
 likes = json.loads(likes.text)
