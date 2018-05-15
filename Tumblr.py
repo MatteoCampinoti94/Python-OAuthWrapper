0c24c0960635ee14371e3a5b6bac3d137a689c66
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-05-15 21:05:34 +0200

    New function get_tokens() to request oauth tokens, use tokens for get()

--- Tumblr.py
+++ Tumblr.py
@@ -1,38 +1,49 @@
 import requests
+import oauth2
+from requests_oauthlib import OAuth1
+import urllib.parse as urlparse
 import json
 import os
 
 class Tumblr:
-    def __init__(self, api_key='', api_sec='', oauth_token='', oauth_sec='', file='tumblr.conf.json', quiet=True):
-        if type(api_key) != str or type(api_sec) != str:
+    def __init__(self, oauth_key='', oauth_key_sec='', oauth_token='', oauth_token_sec='', file='tumblr.conf.json', quiet=True):
+        if type(oauth_key) != str or type(oauth_key_sec) != str:
             raise TypeError('Api keys need to be passed as strings')
-        if type(oauth_token) != str or type(oauth_sec) != str:
+        if type(oauth_token) != str or type(oauth_token_sec) != str:
             raise TypeError('OAuth tokens need to be passed as strings')
         if type(file) != str:
             raise TypeError('Conf file needs to be passed as string')
         if type(quiet) != bool:
             raise TypeError('quiet argument needs to be of type bool')
 
-        if os.path.isfile(file) and api_key == '':
-            with open(file) as conf:
+        if os.path.isfile(file) and oauth_key == '':
+            with open(file, 'r') as conf:
                 try:
                     conf = json.load(conf)
-                    api_key = conf.get('api_key', '')
-                    api_sec = conf.get('api_sec', '')
+                    oauth_key = conf.get('oauth_key', '')
+                    oauth_key_sec = conf.get('oauth_key_sec', '')
                     oauth_token = conf.get('oauth_token', '')
-                    oauth_sec = conf.get('oauth_sec', '')
+                    oauth_token_sec = conf.get('oauth_token_sec', '')
                 except:
                     raise TypeError(f'"{file}" contains errors')
 
 
-        self.api_key = api_key
-        self.api_sec = api_sec
-        self.api_keys = (self.api_key, self.api_sec)
+        self.oauth_key = oauth_key
+        self.oauth_key_sec = oauth_key_sec
+        self.oauth_keys = (self.oauth_key, self.oauth_key_sec)
         self.oauth_token = oauth_token
-        self.oauth_sec   = oauth_sec
-        self.oauth_tokens = (self.oauth_token, self.oauth_sec)
+        self.oauth_token_sec   = oauth_token_sec
+        self.oauth_tokens = (self.oauth_token, self.oauth_token_sec)
 
-        if api_key == '':
+        if self.oauth_token and self.oauth_token_sec:
+            self.oauth = OAuth1(self.oauth_key, self.oauth_key_sec, self.oauth_token, self.oauth_token_sec)
+        else:
+            self.oauth = None
+
+        if bool(self.oauth_token) + bool(self.oauth_token) == 1:
+            raise TypeError('Only one token was provided, needs both')
+
+        if oauth_key == '':
             raise TypeError('Consumer key cannot be empty')
 
         if not quiet:
@@ -40,10 +51,10 @@ class Tumblr:
             self.tokens()
 
     def keys(self):
-        print(f'Consumer key = {self.api_key}\nConsumer secret key = {self.api_sec}')
+        print(f'Consumer key = {self.oauth_key}\nConsumer secret key = {self.oauth_key_sec}')
 
     def tokens(self):
-        print(f'OAuth token = {self.oauth_token}\nOAuth secret token = {self.oauth_sec}')
+        print(f'OAuth token = {self.oauth_token}\nOAuth secret token = {self.oauth_token_sec}')
 
     def get(self, user, section, limit=20, offset=0, before=0, after=0):
         if type(user) != str or type(section) != str:
@@ -54,8 +65,7 @@ class Tumblr:
             raise TypeError('Can only pass either offset, before or after')
 
         url  = f"http://api.tumblr.com/v2/blog/{user}.tumblr.com/{section}"
-        url += f"?api_key={self.api_key}"
-        url += f"&limit={limit}"
+        url += f"?limit={limit}"
         if offset > 0:
             url += f"&offset={offset}"
         elif before > 0:
@@ -63,7 +73,10 @@ class Tumblr:
         elif after > 0:
             url += f"&after={after}"
 
-        get = requests.get(url)
+        if self.oauth:
+            get = requests.get(url, auth=self.oauth)
+        else:
+            get = requests.get(url+f"?oauth_key={self.oauth_key}")
         get = json.loads(get.text)
         get = {
             'user': user,
@@ -74,6 +87,45 @@ class Tumblr:
             }
 
         return get
+
+    def get_tokens(self):
+        tokenurl_request = 'http://www.tumblr.com/oauth/request_token'
+        tokenurl_authorize = 'http://www.tumblr.com/oauth/authorize'
+        tokenurl_access = 'http://www.tumblr.com/oauth/access_token'
+
+        oauth_key = self.oauth_key
+        oauth_key_sec = self.oauth_key_sec
+
+        consumer = oauth2.Consumer(oauth_key, oauth_key_sec)
+        client = oauth2.Client(consumer)
+
+        response, content = client.request(tokenurl_request, "GET")
+        content = content.decode()
+        request_token = dict(urlparse.parse_qsl(content))
+
+        print("Go to the following link in your browser:")
+        print("{tokenurl_authorize}?oauth_token={request_token['oauth_token']}")
+        oauth_verifier = input('Paste the oaut_verifier: ')
+
+        token = oauth2.Token(request_token['oauth_token'], request_token['oauth_token_secret'.])
+        token.set_verifier(oauth_verifier)
+        client = oauth2.Client(consumer, token)
+
+        response, content = client.request(tokenurl_access, "GET")
+        content = content.decode()
+        access_token = dict(urlparse.parse_qsl(content))
+
+        self.oauth_token = access_token.get('oauth_token', '')
+        self.oauth_token_sec = access_token.get('oauth_token_secret', '')
+        self.oauth_tokens = (self.oauth_token, self.oauth_token_sec)
+
+        if self.oauth_token and self.oauth_token_sec:
+            self.oauth = OAuth1(self.oauth_key, self.oauth_key_sec, self.oauth_token, self.oauth_token_sec)
+        else:
+            self.oauth = None
+
+        if not quiet:
+            self.tokens()
 
     def get_tot(self, user, section):
         if section.startswith('posts'):
