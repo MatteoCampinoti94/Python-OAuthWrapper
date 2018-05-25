496e02bb0dc21c72a6829c2a66dee5fb145e154d
MatteoCampinoti94 <matteo.campinoti94@gmail.com>
2018-05-25 10:47:25 +0200

    Change get() to api_request() for general usage

--- Tumblr/TumblrClass.py
+++ Tumblr/TumblrClass.py
@@ -1,8 +1,9 @@
 import requests
 from requests_oauthlib import OAuth1, OAuth1Session
+from urllib.parse import urlencode
 import json
 
-class Tumblr:
+class TumblrBase:
     def __init__(self, oauth_key='', oauth_key_sec='', oauth_token='', oauth_token_sec='', file='tumblr.conf.json', quiet=True):
         if type(oauth_key) != str or type(oauth_key_sec) != str:
             raise TypeError('Api keys need to be passed as strings')
@@ -26,17 +27,6 @@ class Tumblr:
             if not quiet:
                 self.keys()
                 self.tokens()
-
-    def check_oauth(self):
-        if self.oauth_token and self.oauth_token_sec and (not self.oauth_key or not self.oauth_key_sec):
-            raise TypeError('Needs both oauth consumer keys if tokens are provided')
-        elif bool(self.oauth_token) + bool(self.oauth_token) == 1:
-            raise TypeError('Needs both oauth tokens')
-
-        if self.oauth_key == '':
-            raise TypeError('Consumer key cannot be empty')
-
-        self.oauth = OAuth1(self.oauth_key, self.oauth_key_sec, self.oauth_token, self.oauth_token_sec)
 
     def conf_read(self, file='tumblr.conf.json', quiet=True):
         with open(file, 'r') as conf:
@@ -68,42 +58,16 @@ class Tumblr:
         with open(file, 'w') as conf:
             conf.write(json.dumps(oauth, indent=2)+'\n')
 
-    def keys(self):
-        print(f'Consumer key = {self.oauth_key}\nConsumer secret key = {self.oauth_key_sec}')
+    def check_oauth(self):
+        if self.oauth_token and self.oauth_token_sec and (not self.oauth_key or not self.oauth_key_sec):
+            raise TypeError('Needs both oauth consumer keys if tokens are provided')
+        elif bool(self.oauth_token) + bool(self.oauth_token) == 1:
+            raise TypeError('Needs both oauth tokens')
 
-    def tokens(self):
-        print(f'OAuth token = {self.oauth_token}\nOAuth secret token = {self.oauth_token_sec}')
+        if self.oauth_key == '':
+            raise TypeError('Consumer key cannot be empty')
 
-    def get(self, user, section, limit=20, offset=0, before=0, after=0):
-        if type(user) != str or type(section) != str:
-            raise TypeError('User and section must be passed as strings')
-        if any(type(arg) != int for arg in (limit, offset, before, after)):
-            raise TypeError('limit, offset, before and after arguments need to be integers')
-        if bool(offset) + bool(before) + bool(after) > 1:
-            raise TypeError('Can only pass either offset, before or after')
-
-        self.check_oauth()
-
-        url  = f"http://api.tumblr.com/v2/blog/{user}.tumblr.com/{section}"
-        url += f"?limit={limit}"
-        if offset > 0:
-            url += f"&offset={offset}"
-        elif before > 0:
-            url += f"&before={before}"
-        elif after > 0:
-            url += f"&after={after}"
-
-        get = requests.get(url, auth=self.oauth)
-        get = json.loads(get.text)
-        get = {
-            'user': user,
-            'section': section,
-            'meta': get['meta'],
-            'errors': get.get('errors', [{None: None}]),
-            'response': get.get('response', [{None: None}]),
-            }
-
-        return get
+        self.oauth = OAuth1(self.oauth_key, self.oauth_key_sec, self.oauth_token, self.oauth_token_sec)
 
     def get_tokens(self, save=False, quiet=True):
         tokenurl_request = 'http://www.tumblr.com/oauth/request_token'
@@ -138,3 +102,45 @@ class Tumblr:
             self.conf_save()
         if not quiet:
             self.tokens()
+
+    def keys(self):
+        print(f'Consumer key = {self.oauth_key}\nConsumer secret key = {self.oauth_key_sec}')
+
+    def tokens(self):
+        print(f'OAuth token = {self.oauth_token}\nOAuth secret token = {self.oauth_token_sec}')
+
+    def api_request(url, mode, **params):
+        if type(url) != str or type(mode) != str:
+            raise TypeError('URL and mode must be passed as strings')
+
+        self.check_oauth()
+
+        url = url.rstrip('/') + '/'
+        url_param = urlencode(params)
+        url += '?'*bool(url_param)+url_param
+
+        if mode == 'GET':
+            get = requests.get(url, auth=self.oauth)
+            get = json.loads(get.text)
+            get = {
+                'user': user,
+                'section': section,
+                'meta': get['meta'],
+                'errors': get.get('errors', [{None: None}]),
+                'response': get.get('response', [{None: None}]),
+                }
+
+            return get
+        # Testing
+        # elif mode == 'POST':
+        #     post = requests.post(url, auth=self.oauth)
+        #     post = json.loads(post.text)
+        #     post = {
+        #         'user': user,
+        #         'section': section,
+        #         'meta': get['meta'],
+        #         'errors': get.get('errors', [{None: None}]),
+        #         'response': get.get('response', [{None: None}]),
+        #         }
+        #
+        #     return post
